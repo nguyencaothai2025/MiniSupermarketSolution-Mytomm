3ae1ec88410d99a5a8e9426a6a5cad496b836325
Thai Nguyen Cao <tg_thainc@hiu.vn>
2026-09-19 08:16:36 +0700

    Add project files.

---
+++ MiniSupermarket.API/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿/*
+ *  họ tên: Nguyễn Văn Tommy
+ *  masv: 123456789
+ *  mô tả: Lớp Category đại diện cho nhóm hàng hóa trong siêu thị mini, bao gồm các thuộc tính như mã định danh, tên nhóm hàng và mô tả chi tiết.
+ *  ngày tạo: 2024-06-15
+ */
+namespace MiniSupermarket.API.Models
+{
+    // Lớp biểu diễn thực thể Nhóm hàng hóa trong siêu thị mini
+    public class Category
+    {
+        // Mã định danh nhóm hàng (Khóa chính)
+        public int CategoryId { get; set; }
+
+        // Tên nhóm hàng (Bắt buộc, không được để trống)
+        public string CategoryName { get; set; } = string.Empty;
+
+        // Mô tả chi tiết về nhóm hàng (Có thể để trống)
+        public string? Description { get; set; }
+    }
+}
---
+++ MiniSupermarket.WinForms/FormCategoryManagement.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace MiniSupermarket.WinForms
+{
+    partial class FormCategoryManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            btnLoad = new Button();
+            btnSearch = new Button();
+            txtKeyword = new TextBox();
+            dgvCategories = new DataGridView();
+            groupBox2 = new GroupBox();
+            label3 = new Label();
+            label2 = new Label();
+            txtDescription = new TextBox();
+            txtCategoryName = new TextBox();
+            txtId = new TextBox();
+            label1 = new Label();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvCategories).BeginInit();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnLoad);
+            groupBox1.Controls.Add(btnSearch);
+            groupBox1.Controls.Add(txtKeyword);
+            groupBox1.Location = new Point(51, 32);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(565, 85);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Tìm kiếm";
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(441, 38);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(112, 34);
+            btnLoad.TabIndex = 1;
+            btnLoad.Text = "Tải lại";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(323, 38);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(112, 34);
+            btnSearch.TabIndex = 1;
+            btnSearch.Text = "Tìm kiếm";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // txtKeyword
+            // 
+            txtKeyword.Location = new Point(19, 38);
+            txtKeyword.Name = "txtKeyword";
+            txtKeyword.Size = new Size(288, 31);
+            txtKeyword.TabIndex = 0;
+            txtKeyword.Text = "Nhập từ khóa";
+            // 
+            // dgvCategories
+            // 
+            dgvCategories.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCategories.Location = new Point(41, 129);
+            dgvCategories.Name = "dgvCategories";
+            dgvCategories.RowHeadersWidth = 62;
+            dgvCategories.Size = new Size(575, 298);
+            dgvCategories.TabIndex = 1;
+            dgvCategories.CellClick += dgvCategories_CellClick;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(txtDescription);
+            groupBox2.Controls.Add(txtCategoryName);
+            groupBox2.Controls.Add(txtId);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Location = new Point(670, 48);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(316, 312);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Thông tin nhóm hàng";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(11, 188);
+            label3.Name = "label3";
+            label3.Size = new Size(116, 25);
+            label3.TabIndex = 0;
+            label3.Text = "Mô tả chi tiết";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 102);
+            label2.Name = "label2";
+            label2.Size = new Size(135, 25);
+            label2.TabIndex = 0;
+            label2.Text = "Tên nhóm hàng";
+            // 
+            // txtDescription
+            // 
+            txtDescription.Location = new Point(11, 216);
+            txtDescription.Multiline = true;
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new Size(288, 80);
+            txtDescription.TabIndex = 0;
+            // 
+            // txtCategoryName
+            // 
+            txtCategoryName.Location = new Point(11, 130);
+            txtCategoryName.Name = "txtCategoryName";
+            txtCategoryName.Size = new Size(288, 31);
+            txtCategoryName.TabIndex = 0;
+            // 
+            // txtId
+            // 
+            txtId.Enabled = false;
+            txtId.Location = new Point(11, 57);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(288, 31);
+            txtId.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Mã ID";
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(670, 366);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(102, 34);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "Thêm mới";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(778, 366);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(102, 34);
+            btnUpdate.TabIndex = 1;
+            btnUpdate.Text = "Cập nhật";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(886, 366);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(102, 34);
+            btnDelete.TabIndex = 1;
+            btnDelete.Text = "Xóa";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // FormCategoryManagement
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1020, 492);
+            Controls.Add(groupBox2);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
+            Controls.Add(dgvCategories);
+            Controls.Add(groupBox1);
+            Name = "FormCategoryManagement";
+            Text = "FormCategoryManagement";
+            Load += FormCategoryManagement_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvCategories).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button btnLoad;
+        private Button btnSearch;
+        private TextBox txtKeyword;
+        private DataGridView dgvCategories;
+        private GroupBox groupBox2;
+        private Label label3;
+        private Label label2;
+        private TextBox txtDescription;
+        private TextBox txtCategoryName;
+        private TextBox txtId;
+        private Label label1;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+    }
+}
